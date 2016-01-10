00f6ddf930f7dfc91600c71441ef7aefbfcad2e3
dylanfisher <hi@dylanfisher.com>
2016-01-10 12:56:44 -0500

    Add paperclip gem, add avatar to Users

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem 'rails-admin-scaffold'
 
 gem 'simple_form'
 
+gem 'paperclip', '~> 4.3'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,4 @@
-class Admin::UsersController < ApplicationController
+class Admin::UsersController < Admin::ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /admin/users
@@ -69,6 +69,6 @@ class Admin::UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :user_groups)
+      params.require(:user).permit(:first_name, :last_name, :email, :user_groups, :avatar, :password)
     end
 end
