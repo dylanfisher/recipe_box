f049f50c51624ce666a4a88bb38428ca61e88335
dylanfisher <hi@dylanfisher.com>
2016-08-27 16:20:09 -0400

    Validate cuisine, diet, meal_type, occasion, season by unique title

--- app/models/meal_type.rb
+++ app/models/meal_type.rb
@@ -1,3 +1,5 @@
 class MealType < ActiveRecord::Base
   has_and_belongs_to_many :recipes
+
+  validates_uniqueness_of :title, :case_sensitive => false
 end
--- app/models/occasion.rb
+++ app/models/occasion.rb
@@ -1,3 +1,5 @@
 class Occasion < ActiveRecord::Base
   has_and_belongs_to_many :recipes
+
+  validates_uniqueness_of :title, :case_sensitive => false
 end
